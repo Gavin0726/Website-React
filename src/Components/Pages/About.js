bb3208b54f4876823b3595e17b75e30735cb72f4
gavin <lnguoxun@hotmail.com>
2020-01-27 16:40:08 +1100

    fix about.js

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -10,7 +10,7 @@ class About extends Component {
             buttonText: "",
             link: "/about",
             showButton: false,
-            image: "url('img/about.jpg')"
+            image: "url('img/about1.jpg')"
         }
     }
 
